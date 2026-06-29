0ea3a198b2614a11ec86d7add65a4f8cc809af26
Lichessbotacc <110300918+Lichessbotacc@users.noreply.github.com>
2026-06-29 03:16:33 +0200

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,23 +2,19 @@ export const config = {
   server: "https://lichess.org",
   team: "darkonbullt",
   oauthToken: process.env.OAUTH_TOKEN!,
-  daysInAdvance: 0,
+
   dryRun: false,
 
   arena: {
-    name: () => "Hourly 1+0",
-    description: () => "Professional Bullet tournaments by DarkOnBullt team!",
+    name: () => "DarkOnBullet 1+0",
+    description: () =>
+      "Welcome to the 1+0 Arenas of DarkOnBullet! [JOIN OUR WHATSAPP GROUP](https://chat.whatsapp.com/KAZn4UrhOZq57P4cPjAuYe?s=cl&p=i&mlu=1) , Good luck and invite your friends!",
 
-    clockTime: 1,          // 1+0
+    clockTime: 1,
     clockIncrement: 0,
-    minutes: 120,          // Turnier dauert 2 Stunden
-    rated: true,
-    variant: "standard",
-    intervalHours: 2,      // alle 2 Stunden
-  },
+    minutes: 120,
 
-  schedule: {
-    startHours: [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22],
-    timezone: "UTC",
-  },
+    rated: true,
+    variant: "standard"
+  }
 };
